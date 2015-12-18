04e38cacfffc7a384d0ce53ca3fb1dcd672480b5
Riccardo Maffei <riccardo.maffei@studio.unibo.it>
2015-12-18 09:45:20 +0100

    edited main.cpp documentation

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Danilo Branca
+ * Author: Riccardo Maffei
+ * Author: Federico Bertani
+ *
+ * Created on 15 dicembre 2015, 09.00
+ */
 #include <iostream>
 
 using namespace std;
 
-int main() {
+/**
+ * This is the launch function main. It takes care of the console args and starts the game.
+ */
+int main(int argc, char* argv[]) {
 
     return 0;
 }
