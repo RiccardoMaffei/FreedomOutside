0ce09f9894b8545b2ccaceb9e050beb6650849b2
Riccardo Maffei <riccardo.maffei@studio.unibo.it>
2015-12-18 07:21:52 +0100

    Added some setters and getters to the Room header and improved the documentation.

--- Room.hpp
+++ Room.hpp
@@ -19,26 +19,86 @@ using namespace std;
 class Room {
 public:
     /**
-     * Construct an empty room w/ the given doors (room pointers)
-     * @param x int: the x coordinate
-     * @param y int: the x coordinate
-     * @param north Room*: the northern room
-     * @param south Room*: the southern room
-     * @param east Room*: the eastern room
-     * @param west Room*: the western room
+     * Construct an empty room w/ the given doors (room pointers).
+     * @param x int: the x coordinate.
+     * @param y int: the x coordinate.
+     * @param north Room*: the northern room.
+     * @param south Room*: the southern room.
+     * @param east Room*: the eastern room.
+     * @param west Room*: the western room.
      */
     Room(int x, int y, Room* north, Room* south, Room* east, Room* west);
     
     /**
-     * Clone the given room
-     * @param orig Room&: room to clone
+     * Clone the given room.
+     * @param orig Room&: room to clone.
      */
     Room(const Room& orig);
     
     /**
-     * Room destructor
+     * Room destructor.
      */
     virtual ~Room();
+    
+    /**
+     * Get the east room pointer.
+     * @return Room*: returns the pointer to the east room or NULL if it has not been set yet.
+     */
+    Room* getEast();
+
+    /**
+     * Set the given Room pointer as the east room.
+     * @param east Room*: east room.
+     */
+    void setEast(Room* east);
+
+    /**
+     * Get the north room pointer.
+     * @return Room*: returns the pointer to the north room or NULL if it has not been set yet.
+     */
+    Room* getNorth();
+
+    /**
+     * Set the given Room pointer as the north room.
+     * @param north Room*: north room.
+     */
+    void setNorth(Room* north);
+
+    /**
+     * Get the south room pointer.
+     * @return Room*: returns the pointer to the south room or NULL if it has not been set yet.
+     */
+    Room* getSouth();
+
+    /**
+     * Set the given Room pointer as the south room.
+     * @param south Room*: south room.
+     */
+    void setSouth(Room* south);
+
+    /**
+     * Get the west room pointer.
+     * @return Room*: returns the pointer to the west room or NULL if it has not been set yet.
+     */
+    Room* getWest();
+
+    /**
+     * Set the given Room pointer as the west room.
+     * @param west Room*: west room.
+     */
+    void setWest(Room* west);
+
+    /**
+     * Get the x coordinate of this room.
+     * @return int: x coordinate.
+     */
+    int getX();
+
+    /**
+     * Get the y coordinate of this room.
+     * @return int: y coordinate.
+     */
+    int getY();
     
     /**
      * Returns a lisf of possible actions for the room.
