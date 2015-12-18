4ec330495a290eb98fde29441447c05774a82d83
Riccardo Maffei <riccardo.maffei@studio.unibo.it>
2015-12-18 17:09:18 +0100

    Implemented Room constructor with all parameters (last commit was faulty).

--- Room.cpp
+++ Room.cpp
@@ -8,6 +8,18 @@
 #include "Room.hpp"
 
 Room::Room(int x, int y, Room* north, Room* south, Room* east, Room* west) {
+    //save the given x
+    this -> x = x;
+    //save the given y
+    this -> y = y;
+    //save the given north pointer
+    this -> north = north;
+    //save the given south pointer
+    this -> south = south;
+    //save the given east pointer
+    this -> east = east;
+    //save the given west pointer
+    this -> west = west;
 }
 
 Room::Room(const Room& orig) {
