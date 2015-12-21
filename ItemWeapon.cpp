b00267a113231c9e21a86915f0da5d98f5275e40
Riccardo Maffei <riccardo.maffei@studio.unibo.it>
2015-12-21 21:38:12 +0100

    Added ItemWeapon class.

---
+++ ItemWeapon.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   ItemWeapon.cpp
+ * Author: Riccardo Maffei
+ * 
+ * Created on 21 dicembre 2015, 21.37
+ */
+
+#include "ItemWeapon.hpp"
+
+ItemWeapon::ItemWeapon() {
+}
+
+ItemWeapon::~ItemWeapon() {
+}
+
---
+++ ItemWeapon.hpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   ItemWeapon.hpp
+ * Author: Riccardo Maffei
+ *
+ * Created on 21 dicembre 2015, 21.37
+ */
+
+#ifndef ITEMWEAPON_HPP
+#define ITEMWEAPON_HPP
+
+#include "Item.hpp"
+
+class ItemWeapon: public Item{
+public:
+    ItemWeapon();
+    virtual ~ItemWeapon();
+private:
+
+};
+
+#endif /* ITEMWEAPON_HPP */
+
