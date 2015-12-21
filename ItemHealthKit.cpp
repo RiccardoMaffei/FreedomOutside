2654509fdf14c3391ef9645c4e318a7833bcf935
Riccardo Maffei <riccardo.maffei@studio.unibo.it>
2015-12-21 21:43:22 +0100

    Added ItemHealthKit class.

---
+++ ItemHealthKit.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   ItemHealthKit.cpp
+ * Author: Riccardo Maffei
+ * 
+ * Created on 21 dicembre 2015, 21.42
+ */
+
+#include "ItemHealthKit.hpp"
+
+ItemHealthKit::ItemHealthKit() {
+}
+
+ItemHealthKit::~ItemHealthKit() {
+}
+
---
+++ ItemHealthKit.hpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   ItemHealthKit.hpp
+ * Author: Riccardo Maffei
+ *
+ * Created on 21 dicembre 2015, 21.42
+ */
+
+#ifndef ITEMHEALTHKIT_HPP
+#define ITEMHEALTHKIT_HPP
+
+class ItemHealthKit {
+public:
+    ItemHealthKit();
+    virtual ~ItemHealthKit();
+private:
+
+};
+
+#endif /* ITEMHEALTHKIT_HPP */
+
