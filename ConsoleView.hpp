f3473dcce75247ac1bf60fb5b9d5ceee937846bf
Riccardo Maffei <riccardo.maffei@studio.unibo.it>
2015-12-21 13:12:30 +0100

    Improved ConsoleView documentation.

--- ConsoleView.hpp
+++ ConsoleView.hpp
@@ -8,9 +8,19 @@
 #ifndef CONSOLEVIEW_HPP
 #define CONSOLEVIEW_HPP
 
+/**
+ * Console view for the game.
+ */
 class ConsoleView {
 public:
+    /**
+     * Construct a console view
+     */
     ConsoleView();
+    
+    /**
+     * Console view destructor
+     */
     virtual ~ConsoleView();
 private:
 
