dbc08b711776992a90912ce4f975da990167f1ec
Riccardo Maffei <riccardo.maffei@studio.unibo.it>
2015-12-21 13:16:58 +0100

    Implemented Game contructors.

--- Game.cpp
+++ Game.cpp
@@ -6,12 +6,24 @@
  */
 
 #include "Game.hpp"
+#include "Map.hpp"
 
 Game::Game() {
+    //call the constructor with number of player = 4
+    Game(4);
 }
 
 
 Game::~Game() {
+}
+
+Game::Game(int nPlayer) {
+    //save the number of player
+    this -> nPlayer = nPlayer;
+    //get the map singleton instance
+    this -> map = Map::getInstance();
+    //instance the view
+    this -> view = new ConsoleView();
 }
 
 void Game::play() {
