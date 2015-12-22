f9569daeab1e74c11ca5653a993cf4514877d37e
Riccardo Maffei <riccardo.maffei@studio.unibo.it>
2015-12-22 15:45:26 +0100

    Added Item constructor, members and deleted clone constructor. Improved Item documentation.

--- Item.hpp
+++ Item.hpp
@@ -8,13 +8,25 @@
 #ifndef ITEM_HPP
 #define	ITEM_HPP
 
+/**
+ * An Item. It can be on the ground or held.
+ */
 class Item {
 public:
-    Item();
-    Item(const Item& orig);
+    
+    /**
+     * Construct an Item with the given name.
+     * @param name
+     */
+    Item(char name[]);
+    
+    /**
+     * Item destructor
+     */
     virtual ~Item();
 private:
-
+    //the name of the item
+    char name[50];
 };
 
 #endif	/* ITEM_HPP */
