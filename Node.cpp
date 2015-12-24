396df7aeac9b1697789309d3b487703810dc8af4
Riccardo Maffei <riccardo.maffei@studio.unibo.it>
2015-12-24 04:25:15 +0100

    Improved Node documentation and formatting. Edited Node constructor signature.

--- Node.cpp
+++ Node.cpp
@@ -1,6 +1,7 @@
 /* 
  * File:   Node.cpp
- * Author: fede
+ * Author: Federico Bertani
+ * Author: Riccardo Maffei
  * 
  * Created on 18 dicembre 2015, 14.55
  */
@@ -11,47 +12,58 @@
 #include "Node.hpp"
 
 template <class ListType>
-Node<ListType>::Node(ListType* value,Node* prev,Node* next) {
-    this->value = *value;
-    this->prev = prev;
-    this->next = next;
+Node<ListType>::Node(ListType value, Node* prev, Node* next){
+    //save the value
+    this -> value = value;
+    //save the previous node pointer
+    this -> prev = prev;
+    //save the next node pointer
+    this -> next = next;
 }
 
 template <class ListType>
-Node<ListType>::Node(const Node& orig) {
+Node<ListType>::Node(const Node& orig){
+    //copy the value
     value = orig.value;
+    //copy the previous node pointer
     prev = orig.prev;
+    //copy the next node pointer
     next = orig.next;
 }
 
 template <class ListType>
-Node<ListType>::~Node() {
-
+Node<ListType>::~Node(){
+    //do nothing
 }    
 
 template <class ListType>
-ListType Node<ListType>::getValue() {
+ListType Node<ListType>::getValue(){
+    //return the value
    return value; 
 }
 
 template <class ListType>
-void Node<ListType>::setPrev(Node* node) {
-    this->prev = node;
+void Node<ListType>::setPrev(Node* node){
+    //save the previous node pointer
+    this -> prev = node;
 }
 
 template <class ListType>
-void Node<ListType>::setNext(Node* node) {
-    this->next = node;
+void Node<ListType>::setNext(Node* node){
+    //save the next node pointer
+    this -> next = node;
 }
 
 template <class ListType>
-Node<ListType>* Node<ListType>::getPrev() {
-   return prev; 
+Node<ListType>* Node<ListType>::getPrev(){
+    //return the previous node pointer
+    return this -> prev; 
 }
 
 template <class ListType>
-Node<ListType>* Node<ListType>::getNext() {
-   return next; 
+Node<ListType>* Node<ListType>::getNext(){
+    //return the next node pointer
+    return this -> next; 
 }
 
 #endif	/* NODE_H */
--- Node.hpp
+++ Node.hpp
@@ -1,6 +1,7 @@
 /* 
  * File:   Node.h
- * Author: fede
+ * Author: Federico Bertani
+ * Author: Riccardo Maffei
  *
  * Created on 18 dicembre 2015, 14.55
  */
@@ -11,20 +12,68 @@
 #include <exception>
 #include <stddef.h>
 
+/**
+ * A FedeList node.
+ */
 template <class ListType>
 class Node {
 public:
-    Node(ListType* value,Node* prev=NULL,Node* next=NULL);
+    
+    /**
+     * Construct a node with the given value and next/prev pointers.
+     * @param value ListType: the value.
+     * @param prev Node*: a pointer to the previous node.
+     * @param next Node*: a pointer to the next node.
+     */
+    Node(ListType value, Node* prev = NULL, Node* next = NULL);
+    
+    /**
+     * Clone the given Node.
+     * @param orig Node: the original Node.
+     */
     Node(const Node& orig);
+    
+    /**
+     * Get the Node value.
+     * @return ListType: returns the value of the node.
+     */
     ListType getValue();
+    
+    /**
+     * Set the next Node pointer.
+     * @param node Node*: the next Node.
+     */
     void setNext(Node* node);
+    
+    /**
+     * Set the previous Node pointer.
+     * @param node Node*: the previous Node.
+     */
     void setPrev(Node* node);
+    
+    /**
+     * Get the next Node pointer.
+     * @return Node*: the next Node.
+     */
     Node* getNext();
+    
+    /**
+     * Get the previous Node pointer.
+     * @return Node*: the previous Node.
+     */
     Node* getPrev();
+    
+    /**
+     * Node destructor.
+     */
     virtual ~Node();
+
 private:
+    //the value
     ListType value;
+    //pointer to the next node
     Node* next;
+    //pointer to the previous node
     Node* prev;
 };
 
