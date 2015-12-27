f518a027ac41e5f90eb53ea65b9ccfaa097a8b50
Federico Bertani <federico.bertani96@gmail.com>
2015-12-27 14:59:05 +0100

    Created interface of ActionDrop class.

---
+++ ActionDrop.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   ActionDrop.cpp
+ * Author: fede
+ * 
+ * Created on 27 dicembre 2015, 14.36
+ */
+
+#include "ActionDrop.h"
+
+ActionDrop::ActionDrop(Player* player, Item* itemToDrop) {
+
+}
+
+
+ActionDrop::~ActionDrop() {
+}
+
+void ActionDrop::execute() {
+
+}
+
+void ActionDrop::getDescription(char dest[]) {
+
+}
+
+
+
---
+++ ActionDrop.h
@@ -0,0 +1,49 @@
+/* 
+ * File:   ActionDrop.h
+ * Author: fede
+ *
+ * Created on 27 dicembre 2015, 14.36
+ */
+
+#ifndef ACTIONDROP_H
+#define	ACTIONDROP_H
+
+#include "Action.hpp"
+#include "Player.hpp"
+#include "Item.hpp"
+
+/**
+ * The action to drop an item from the inventory of a player to the floor.
+ */
+class ActionDrop : public Action{
+public:
+    /**
+     * Construct an action to drop an item from the inventory of a player
+     * @param player Player*: the player that drop the item
+     * @param itemToDrop Item*: the item to drop on the floor
+     */
+    ActionDrop(Player* player,Item* itemToDrop);
+    
+    /**
+     * ActionDrop destructor.
+     */
+    virtual ~ActionDrop();
+    
+    /**
+     * Drop the item on the floor of the room.
+     */
+    virtual void execute();
+    
+    /**
+     * Get the Action description. 
+     * @param dest char[]: destination array. Must be big enough (max string length 255).
+     */
+    virtual void getDescription(char dest[]);
+
+private:
+    Player* player;
+    Item* itemToDrop;
+};
+
+#endif	/* ACTIONDROP_H */
+
