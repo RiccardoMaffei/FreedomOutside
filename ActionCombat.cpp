fd4c6316c3f7d20f52b790a9388d4fc1bf7e493e
Riccardo Maffei <riccardo.maffei@studio.unibo.it>
2015-12-27 17:43:01 +0100

    Implemented ActionCombat.

--- ActionCombat.cpp
+++ ActionCombat.cpp
@@ -5,14 +5,34 @@
  * Created on 27 dicembre 2015, 14.58
  */
 
+#include <math.h>
+
 #include "ActionCombat.hpp"
 
-ActionCombat::ActionCombat() {
-}
-
-ActionCombat::ActionCombat(const ActionCombat& orig) {
+ActionCombat::ActionCombat(Player* attacker, ItemWeapon* weapon, FedeList<Player*>* listOfPlayers){
+    //save the attacker
+    this -> attacker = attacker;
+    //save the weapon
+    this -> weapon = weapon;
+    //save the list of attacked players
+    this -> attackedPlayers = attackedPlayers;
 }
 
 ActionCombat::~ActionCombat() {
+    //do nothing
 }
 
+void ActionCombat::execute(){
+    //for all attacked players
+    for (int i = 0; i < (this -> attackedPlayers -> getSize()); i++) {
+        //compute damage (it may vary. E.g.: in destruptive weapon)
+        int damage = (this -> attacker -> getStrength()) * (this -> weapon -> getDamageValue());
+        //if the attacked is the attacker
+        if (this -> attacker == this -> attackedPlayers -> get(i)){
+            //divide the damage by the agility
+            damage = floor(damage / (this -> attacker -> getAgility()));
+        }
+        //damage the item
+        this -> attackedPlayers -> get(i) -> damage(damage);
+    }
+}
--- ActionCombat.hpp
+++ ActionCombat.hpp
@@ -9,15 +9,41 @@
 #define ACTIONCOMBAT_HPP
 
 #include "Action.hpp"
+#include "ItemWeapon.hpp"
+#include "FedeList.hpp"
+#include "Player.hpp"
 
-
+/**
+ * A combat action. This action applies damage to a list player with a given weapon using attacker statistics.
+ */
 class ActionCombat: public Action{
 public:
-    ActionCombat();
-    ActionCombat(const ActionCombat& orig);
+    
+    /**
+     * Construct an ActionCombat w/ the given data.
+     * @param attacker Player*: the attacker.
+     * @param weapon ItemWeapon*: the weapon to use for the combat.
+     * @param attackedPlayers FedeList&lt;Player*&gt;*: the list of attacked players.
+     */
+    ActionCombat(Player* attacker, ItemWeapon* weapon, FedeList<Player*>* attackedPlayers);
+    
+    /**
+     * ActionCombat destructor.
+     */
     virtual ~ActionCombat();
-private:
+    
+    /**
+     * Execute the action.
+     */
+    virtual void execute();
 
+private:
+    //the list of attacked players
+    FedeList<Player*>* attackedPlayers;
+    //the attacker
+    Player* attacker;
+    //the weapon
+    ItemWeapon* weapon;
 };
 
 #endif /* ACTIONCOMBAT_HPP */
